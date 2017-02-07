8300bd675c2c857ec98bc4ffad393a6c469d6e66
Akshay Pai <sadypai@gmail.com>
2017-02-07 22:39:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var io = require('socket.io').listen(app.listen(process.env.PORT || 5000));
 
 io.sockets.on('connection', function (socket) {
     socket.on('send', function (data) {
-        data.host = socket.handshake.headers.host;
+        data.host = socket.conn.remoteAddress;
         io.sockets.emit('message', data);
     });
 });
