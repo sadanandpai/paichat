0d6b0d291c17188b83a70d9831aad4cfad31093f
Akshay Pai <sadypai@gmail.com>
2017-09-22 16:06:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ app.get("/", function(req, res){
 });
 
 var io = require('socket.io').listen(app.listen(process.env.PORT || 5000));
+var counter = 0;
 
 io.sockets.on('connection', function (socket) {
+    counter++;
+    io.sockets.emit('counter', data);
     socket.on('send', function (data) {
         data.host = socket.conn.remoteAddress;
         io.sockets.emit('message', data);
