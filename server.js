bf6cc1de1e7ca47b3d7c2a96304997ecd5a4ab32
Akshay Pai <sadypai@gmail.com>
2020-02-18 18:52:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,28 +9,27 @@ app.get("/", function(req, res){
 var io = require('socket.io').listen(app.listen(process.env.PORT || 5000));
 var counter = 0;
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function(socket) {
+  io.sockets.emit("counter", ++counter);
 
-	io.sockets.emit('counter', ++counter);
+  socket.on("ferret", function(name, fn) {
+    fn(socket.id);
+  });
 
-	socket.on('ferret', function (name, fn) {
-    	fn(socket.id);
-  	});
+  socket.on("send", function(data) {
+    data.host = socket.conn.remoteAddress;
+    io.sockets.emit("message", data);
+  });
 
-    socket.on('send', function (data) {
-        data.host = socket.conn.remoteAddress;
-        io.sockets.emit('message', data);
-    });
+  socket.on("exit", function(data) {
+    io.sockets.emit("exit", data);
+  });
 
-    socket.on('exit', function(data){
-    	io.sockets.emit('exit', data);
-  	});
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data);
+  });
 
-  	socket.on('typing', function(data){
-    	socket.broadcast.emit('typing', data);
-  	});
-
-    socket.on('disconnect', function(){
-    	io.sockets.emit('counter', --counter);
-  	});
+  socket.on("disconnect", function() {
+    io.sockets.emit("counter", --counter);
+  });
 });
